aaa233c35296b487a5307fd558e354b81e1fd59f
pm177 <pm@sixpl.com>
2026-09-12 02:14:04 +0530

    Update SectionHeaderComponent.tsx

--- src/components/reuseable/SectionHeaderComponent.tsx
+++ src/components/reuseable/SectionHeaderComponent.tsx
@@ -29,7 +29,7 @@ const DefaultSectionHeader = ({
           </p>
         </div>
       )}
-      <h1 className={`${alignText}`}>{heading}</h1>
+      <h2 className={`${alignText}`}>{heading}</h2>
 
       <p className={`max-w-xl ${alignText}`}>{subHeading}</p>
     </div>
