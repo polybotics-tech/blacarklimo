7eca65835fa12d5a7622b3521346959d815b74c3
pm177 <pm@sixpl.com>
2026-09-12 02:12:48 +0530

    Update TrustBuildingSection.tsx

--- src/components/inpage/TrustBuildingSection.tsx
+++ src/components/inpage/TrustBuildingSection.tsx
@@ -51,7 +51,6 @@ const TrustBuildingSection = ({ screen }: TrustBuildingSectionProps) => {
           <DefaultSectionHeader
             align="left"
             title="Why Choose Us"
-            headingLevel="h2"
             heading={
               <>
                 <span className="text-pri-gold">Excellence</span> In Every Mile
